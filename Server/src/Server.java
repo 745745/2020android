570d7a6791a57c4bd30fbc1c074f984419d0ab24
745745 <41120147+745745@users.noreply.github.com>
2020-07-07 17:46:47 +0800

    2020.7.7 V1.0
    服务端程序完成，相关的接口会上传到群上

---
+++ Server/src/Server.java
@@ -0,0 +1,57 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args)
+    {
+        serverSocket serversocket=new serverSocket();
+        serversocket.run();
+
+    }
+
+
+}
+
+class serverSocket extends Thread
+{
+    private ServerSocket ss;
+    private Socket socket;
+
+    public serverSocket()
+    {
+        try
+        {
+            ss=new ServerSocket(12345);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while(true)
+        {
+            try {
+                socket=ss.accept();
+                System.out.println("已连接到");
+                DataInputStream dis=new DataInputStream(socket.getInputStream());
+                DataOutputStream dos=new DataOutputStream(socket.getOutputStream());
+                while(true)
+                {
+                    String x=dis.readUTF();
+                    String a=analysis.getResult(x);
+                    dos.writeUTF(a);
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
